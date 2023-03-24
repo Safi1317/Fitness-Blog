358687781b4c14ed6acb244168a535e1b58eea13
RileyAlt <rileyaltenburg@gmail.com>
2023-03-23 20:03:25 -0500

    logout page

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const userLogout = async () => {
+
+    const userResponse = await fetch ('/api/user', {
+        method: 'POST',
+        headers: {'Content-Type': ''}
+    });
+
+    if (userResponse.ok){
+
+        document.location.replace('/login');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout')
