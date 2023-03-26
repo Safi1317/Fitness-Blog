6dff3261b748cbbc95d6b32215bcd76ccff22fe5
Safia Ali <ali.swaphiyah@gmail.com>
2023-03-26 07:58:46 -0500

    added user seeds

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Post extends Model { }
+class Post extends Model {}
 
 Post.init(
   {
@@ -11,7 +11,7 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    Category: {
+    category: {
       type: DataTypes.STRING,
       allowNull: false,
     },
