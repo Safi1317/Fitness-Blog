e014f450179a43e11ed047f199173367709f2806
Safia Ali <ali.swaphiyah@gmail.com>
2023-03-28 18:34:59 -0500

    corrected login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -68,7 +68,7 @@ router.get("/workout/:id", async (req, res) => {
 
 router.get("/login", (req, res) => {
   console.log("hit login route");
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
