80277668efa84a930c44c33e5848937a2e322fa9
RileyAlt <rileyaltenburg@gmail.com>
2023-03-27 19:01:06 -0500

    2fa fixes and package updates

--- controllers/2fa.js
+++ controllers/2fa.js
@@ -1,76 +1,74 @@
-// const router = require('express').Router();
-// const speakeasy = require('speakeasy');
-// const uuid = require('uuid');
-// const { User } = require('../models');
-
-// app.use(express.json())
-
-// app.get('/api', (req, res) => res.json({ message: 'Two factor auth begin'}))
-
-// // register user 
-// app.post('api/register', (req, res) => {
-//   const id = uuid.v4()
-
-//   try {
-//       const path = `user${id}`
-//       const tempSecret = speakeasy.generateSecret()
-//       db.push(path, {id, tempSecret})
-//       res.json({id, secret: tempSecret.base32})
-//   } catch (error) {
-//       console.log(error)
-//       res.status(500).json({message: 'Error secret not generated'})
-//   }
-// })
-
-// // verify token
-// app.post ('/api/verify', (req, res) => { 
-//   const {token, userId} = req.body
-
-//   try {
-//       const path = `/user/${userId}`
-//       const user = db.getData(path)
-
-//       const {base32:secret} = user.tempSecret
-
-//       const verified = speakeasy.totp.verify({ secret, 
-//           encoding: 'base32',
-//           token });
-
-//       if(verified) {
-//           db.push(path, {id: userId, secret: user.tempSecret})
-//           res.json({ verified: true })
-//       } else {
-//         res.json ({verified: false})
-//       }
-//   } catch (error){
-//     console.log(error)
-//     res.status(500).json({message: 'Error could not find user'})
-//   }
-// })
-
-// // validate token
-// app.post ('/api/validate', (req, res) => { 
-//   const {token, userId} = req.body
-
-//   try {
-//       const path = `/user/${userId}`
-//       const user = db.getData(path)
-
-//       const {base32:secret} = user.secret
-
-//       const tokenValidate = speakeasy.totp.verify({ secret, 
-//           encoding: 'base32',
-//           token, window: 1  });
-
-//       if(tokenValidate) {
-//           res.json({ validated: true })
-//       } else {
-//         res.json ({validated: false})
-//       }
-//   } catch (error){
-//     console.log(error)
-//     res.status(500).json({message: 'Error could not find user'})
-//   }
-// })
-
-// module.exports = router;
+"use strict";
+//Generating a Secret
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var speakeasy_1 = require("speakeasy");
+var generateUserSecret = function (userRepo, twoFactor) {
+    var secret = (0, speakeasy_1.generateSecret)({
+        otpauth_url: true
+    });
+    twoFactor.secret = secret.base32;
+    userRepo.save(twoFactor);
+    return secret;
+};
+//Displaying QR Code 
+var qrcode_1 = require("qrcode");
+var generateQRCode = function (secret) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, (0, qrcode_1.toDataURL)(secret.otpauth_url)];
+            case 1: return [2 /*return*/, _a.sent()];
+        }
+    });
+}); };
+//Verifiying the Token
+var speakeasy_2 = require("speakeasy");
+var verifyToken = function (userToken, serverSecret) {
+    var verified = speakeasy_2.totp.verify({
+        secret: serverSecret,
+        encoding: 'base32',
+        token: userToken
+    });
+    return verified;
+};
+var enableTwoFactor = function (verified, repo, twoFactor) {
+    if (!twoFactor.enabled) {
+        twoFactor.enabled = verified;
+    }
+    repo.save(twoFactor);
+};
---
+++ controllers/2fa.ts
@@ -0,0 +1,61 @@
+//Generating a Secret
+
+import { generateSecret } from 'speakeasy'
+
+interface TwoFactorEntity {
+
+    userId: number
+    secret: string
+    enabled: boolean 
+}
+
+const generateUserSecret = 
+(userRepo: Repository, twoFactor: TwoFactorEntity) => {
+
+    const secret = generateSecret({
+        otpauth_url: true 
+    })
+        twoFactor.secret = secret.base32
+        userRepo.save(twoFactor)
+        return secret
+}
+
+interface Repository {
+save: (d: TwoFactorEntity) => any; 
+}
+
+
+//Displaying QR Code 
+
+import { toDataURL } from 'qrcode'
+
+interface SecretData {
+    otpauth_url: String
+}
+
+const generateQRCode = async (secret: SecretData) => {
+    return await toDataURL(secret.otpauth_url)
+}
+
+//Verifiying the Token
+
+import { totp } from 'speakeasy'
+
+const verifyToken = (userToken: string, serverSecret: string) : boolean => {
+    const verified = totp.verify({
+        secret: serverSecret,
+        encoding: 'base32',
+        token: userToken
+    })
+    return verified
+}
+
+const enableTwoFactor = (
+    verified: boolean,
+    repo: Repository,
+    twoFactor: TwoFactorEntity) => {
+        if (!twoFactor.enabled) {
+            twoFactor.enabled = verified
+        }
+        repo.save(twoFactor)
+    }
