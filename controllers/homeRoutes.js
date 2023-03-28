398c90846b436bd97cea7a657d8c375bd8551e9c
aliyajeylani <aliya.jeylani24@gmail.com>
2023-03-27 21:57:56 -0500

    working on routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Workout, Comment, User, Activity } = require("../models");
 // const withAuth = require("../utils/auth");
 
+//GET all activites for homepage
 router.get("/", async (req, res) => {
   try {
     const dbActivityData = await Activity.findAll({});
@@ -18,6 +19,69 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//GET all workouts for activity
+router.get("/activity/:id", async (req, res) => {
+  try {
+    const dbActivityData = await Activity.findByPk(req.params.id, {
+
+      include: [
+        {
+          model: Workout,
+          attributes: [
+            'id',
+            'name',
+            'description',
+            'difficulty_level',
+            'equipment',
+            'reps',
+            'username',
+            'filename'
+          ],
+        },
+      ],
+
+    });
+
+    const activity = dbActivityData.get({ plain: true });
+    res.render('workout', { workouts: activity.workouts });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET one workout
+router.get("/workout/:id", async (req, res) => {
+  try {
+    const dbWorkoutData = await Workout.findByPk(req.params.id, {
+
+    });
+
+    if (!dbWorkoutData) {
+      res.status(400).json({ message: 'No Workout found with that id' });
+      return;
+    }
+
+
+
+    // res.status(200).json(dbWorkoutData)
+    // const workoutData = dbActivityData.get({ plain: true });
+    console.log(dbWorkoutData);
+    res.render('workout', { workouts: [dbWorkoutData.dataValues] });
+
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
+
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
