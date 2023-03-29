758562ed162e1213df27c3dad21f8fb28311949d
Safia Ali <ali.swaphiyah@gmail.com>
2023-03-28 20:54:27 -0500

    fixed logout route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,6 +13,7 @@ router.get("/", async (req, res) => {
 
     res.render("homepage", {
       activities,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -73,7 +74,7 @@ router.get("/login", (req, res) => {
     return;
   }
 
-  res.render("login");
+  res.render("login", { loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;
