01250869caa4cd6a69ddded79e14a96d20364f2b
RileyAlt <rileyaltenburg@gmail.com>
2023-03-30 14:26:47 -0500

    data work

--- public/js/workoutRecommender.js
+++ public/js/workoutRecommender.js
@@ -5,14 +5,18 @@ const recommendWorkout = async () => {
   })
     .then((res) => res.json())
     .then((resAsJson) => {
-      console.log("resAsJson = ", resAsJson);
+    //   console.log("resAsJson = ", resAsJson);
 
       //   TODO: show some element on the screen with the data from resAsJson
-      for (const scoreRecord of highScores) {
-        var li = document.createElement('li');
-        li.textContent = `${scoreRecord.name} ... ${scoreRecord.score}`;
-        scoreList.appendChild(li);
-    }
+      const data = JSON.parse(resAsJson);
+
+      const container = document.getElementById('data-container');
+
+      data.forEach(item => {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = item.name, item.type;
+        container.appendChild(paragraph);
+      });
       
       alert(resAsJson[0].name);
     });
