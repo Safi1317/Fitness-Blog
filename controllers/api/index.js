bfbe6055e737ba1e1178c456954df7837be974f2
RileyAlt <rileyaltenburg@gmail.com>
2023-03-29 22:38:40 -0500

    New tech api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const fetch = require("node-fetch");
 
 router.use("/users", userRoutes);
+
+router.get("/get-workouts", (request, response, next) => {
+  //  TODO: change biceps with request.value
+  fetch("https://api.api-ninjas.com/v1/exercises?muscle=biceps", {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": "m1kzM2YOAM4ge5YHsdOGyQ==j8BUSIGy53nP4hfg",
+    },ç
+  })
+    .then((response) => response.json())
+    .then((json) => response.status(200).json(json));
+});
 
 module.exports = router;
